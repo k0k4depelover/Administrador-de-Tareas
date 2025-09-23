f2ae3121ee889285281cf1b16caeb3b8c9f95cf3
Oskar <oskarcermeno1@gmail.com>
2025-09-23 07:49:14 -0600

    Implementacion de cabecera de los metodos a utilizar

---
+++ src/ProcessManager.h
@@ -0,0 +1,13 @@
+#ifndef PROCESSMANAGER_H
+#define PROCESSMANAGER_H
+#include <string>
+#include <vector>
+class ProcessManager{
+    public:
+    int runProcess(const std::string& cmd);
+    bool killProcess(int pid);
+    std::vector<std::string> splitCommand(const std::string& cmd);
+    bool listProcesses();
+    bool isShortCommand(const std::string& cmd);
+};
+#endif // PROCESSMANAGER_HW
